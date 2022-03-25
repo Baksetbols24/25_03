e948b5f7cbffdd0978b873ffce9a9d138ecc3f30
Baksetbols24 <102351168+Baksetbols24@users.noreply.github.com>
2022-03-25 10:02:44 +0200

    Add files via upload

---
+++ RB/script.js
@@ -0,0 +1,9 @@
+let sveiciens = 'Kā iet?';
+let atbilde = 'normali';
+let skaitlis = 10;
+let skaitlis = 100;
+
+console.log(sveiciens);
+console.log(atbilde);
+console.log(skaitlis);
+console.log(skaitlis);
